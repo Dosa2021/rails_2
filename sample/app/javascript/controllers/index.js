5b8a95dfbbc44645344eeb7903dd8d520ea47092
iret-tatsuya-sugawara <tatsuya.sugawara@iret.co.jp>
2024-08-05 08:46:26 +0900

    モーダル実装

--- sample/app/javascript/controllers/index.js
+++ sample/app/javascript/controllers/index.js
@@ -11,7 +11,19 @@ eagerLoadControllersFrom("controllers", application)
 // lazyLoadControllersFrom("controllers", application)
 
 const open = document.querySelector('#open')
+const close = document.querySelector('#close')
+const mask = document.querySelector('#mask')
 
 open.addEventListener('click', () => {
-    console.log('clicked-----')
+    modal.classList.remove('hidden')
+    mask.classList.remove('hidden')
+})
+
+close.addEventListener('click', () => {
+    modal.classList.add('hidden')
+    mask.classList.add('hidden')
+})
+
+mask.addEventListener('click', () => {
+    close.click()
 })
