5747b3d13f24f4b7cecaa2c71f014df950964b05
iret-tatsuya-sugawara <tatsuya.sugawara@iret.co.jp>
2024-08-15 19:47:10 +0900

    ユーザ登録失敗時にエラーメッセージ表示

--- sample/app/models/user.rb
+++ sample/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
-    validates :name, presence: true
+    validates :name, presence: true, length: { maximum: 10 }
+    validates :email, presence: true
     has_secure_password
 end
