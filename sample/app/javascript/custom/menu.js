73ece853ee4fc987a6c7b32787e1587eb65a0835
iret-tatsuya-sugawara <tatsuya.sugawara@iret.co.jp>
2024-08-17 14:03:10 +0900

    メニューのトグル実装

--- sample/app/javascript/custom/menu.js
+++ sample/app/javascript/custom/menu.js
@@ -1,1 +1,10 @@
-console.log('hoge------------')
+document.addEventListener("turbo:load", function() {
+    let account = document.querySelector("#account");
+    if (account) {
+        account.addEventListener("click", function(event) {
+            event.preventDefault();
+            let menu = document.querySelector("#dropdown-menu");
+            menu.classList.toggle("active");
+        })
+    }
+})
