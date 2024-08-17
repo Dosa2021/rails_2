568daf35295b0c26de835c916b8c3d396258265b
iret-tatsuya-sugawara <tatsuya.sugawara@iret.co.jp>
2024-08-17 14:32:09 +0900

    ログアウト実装

--- sample/app/controllers/users_controller.rb
+++ sample/app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # flash[:success] = "Welcome to the Sample App!"
+      reset_session
+      log_in @user
       flash[:success] = "新規ユーザーを作成しました。"
       redirect_to @user
     else
