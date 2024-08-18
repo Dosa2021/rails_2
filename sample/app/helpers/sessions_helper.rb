90db206012dc15ce31187aa3b800aeb90c107c2f
iret-tatsuya-sugawara <tatsuya.sugawara@iret.co.jp>
2024-08-19 08:58:29 +0900

    セッションヘルパーにrememberメソッド定義

--- sample/app/helpers/sessions_helper.rb
+++ sample/app/helpers/sessions_helper.rb
@@ -3,6 +3,13 @@ module SessionsHelper
         session[:user_id] = user.id
     end
 
+    def remember(user)
+        puts 'remember-----'
+        # user.remember
+        # cookies.permanent.encrypted[:user_id] = user.id
+        # cookies.permanent[:remember_token] = user.remember_token
+    end
+
     def current_user
         if session[:user_id]
             @current_user ||= User.find_by(id: session[:user_id])
