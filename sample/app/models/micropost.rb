b8ae0b26d524e8f5a59d4889a20928b8a690a95e
iret-tatsuya-sugawara <tatsuya.sugawara@iret.co.jp>
2024-09-08 18:42:33 +0900

    画像のリサイズ

--- sample/app/models/micropost.rb
+++ sample/app/models/micropost.rb
@@ -1,6 +1,8 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-  has_one_attached :image
+  has_one_attached :image do |attachable|
+    attachable.variant :display, resize_to_limit: [500, 500]
+  end
 
   # ラムダ（lambda）の省略記法（->）
   default_scope -> { order(created_at: :desc) }
--- sample/config/application.rb
+++ sample/config/application.rb
@@ -19,5 +19,6 @@ module Sample
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.default_locale = :ja
+    config.active_storage.variant_processor = :mini_magick
   end
 end
