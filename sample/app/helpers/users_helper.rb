70edcc3bbab752d86300e17a0e7f4e3905f90e2a
iret-tatsuya-sugawara <tatsuya.sugawara@iret.co.jp>
2024-11-18 15:58:41 +0900

    enumからセレクトボックス作成1

--- sample/app/helpers/users_helper.rb
+++ sample/app/helpers/users_helper.rb
@@ -5,4 +5,12 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
+
+  def options_for_select_from_enum(klass, column)
+    enum_list = klass.send(column.to_s.pluralize)
+    enum_list.map do | name , _value |
+      # selectで使うための組み合わせ順は[ 表示値, value値 ]のため以下の通り設定
+      [ t("enums.#{klass.to_s.downcase}.#{column}.#{name}") , name ]
+    end
+  end
 end
